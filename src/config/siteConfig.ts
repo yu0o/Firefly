453aac5d4193d7658f4d432932a72fbdfd369caa
yu0o <lyh@hihh.cn>
2026-09-05 01:35:24 +0800

    no message

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -89,7 +89,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/images/icon.svg",
+			src: "https://xyu.homes/favicon.ico",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
@@ -110,7 +110,7 @@ export const siteConfig: SiteConfig = {
 		// 使用 Astro 图标库时不需要设置 valueDark，图标会自动跟随主题亮暗色切换
 		logo: {
 			type: "image",
-			value: "/assets/images/icon.svg",
+			value: "favicon/icon.svg",
 			// valueDark: "assets/images/logo/firefly-dark.png",
 			alt: "Logo",
 		},
