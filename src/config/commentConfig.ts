f22042b658607b8428db2518ed025011c6af42a4
yu0o <lyh@hihh.cn>
2026-09-05 20:46:48 +0800

    no message

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -15,7 +15,7 @@ export const commentConfig: CommentConfig = {
 		// 中国推荐1: https://registry.npmmirror.com/twikoo/1.7.14/files/dist/twikoo.min.js
 		// 中国推荐2: https://s4.zstatic.net/npm/twikoo@1.7.14/dist/twikoo.min.js
 		// 国际推荐: https://cdn.jsdelivr.net/npm/twikoo@1.7.14/dist/twikoo.min.js
-		jsUrl: "https://cdn.jsdelivr.net/npm/twikoo@1.7.20/dist/twikoo.min.js",
+		jsUrl: "https://cdn.jsdelivr.net/npm/twikoo@1.7.14/dist/twikoo.min.js",
 		// Twikoo 自定义 CSS 文件地址，为空则不加载
 		cssUrl: "/assets/css/twikoo-custom.css",              
 	},
