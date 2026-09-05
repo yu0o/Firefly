7a8b27b1e1b2ef43a1256801633abff36d9e76d8
yu0o <lyh@hihh.cn>
2026-09-05 20:08:08 +0800

    no message

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -6,7 +6,7 @@ export const commentConfig: CommentConfig = {
 
 	//twikoo评论系统配置
 	twikoo: {
-		envId: "https://chat.xyu.homes",
+		envId: "https://twikoo.xyu.homes",
 		// 设置 Twikoo 评论系统语言
 		lang: "zh-CN",
 		// 是否启用文章访问量统计功能
@@ -23,7 +23,7 @@ export const commentConfig: CommentConfig = {
 	//waline评论系统配置
 	waline: {
 		// waline 后端服务地址
-		serverURL: "https://twikoo.xyu.homes",
+		serverURL: "https://waline.xyu.homes",
 		// 设置 Waline 评论系统语言
 		lang: "zh-CN",
 		// 设置 Waline 评论系统表情地址
